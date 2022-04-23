2f6e390227ca161412c08d416ebcf4c66b03a877
Big Andy <8012398+big-andy-coates@users.noreply.github.com>
2022-04-23 01:15:35 +0100

    Update Jackson version

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,8 +71,8 @@ subprojects {
         set("slf4jVersion", "1.7.36")           // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
         set("log4jVersion", "2.17.2")           // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core
 
-        set("jacksonVersion", "2.13.1")         // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
-        set("guavaVersion", "31.1-jre")       // https://mvnrepository.com/artifact/com.google.guava/guava
+        set("jacksonVersion", "2.13.2")         // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
+        set("guavaVersion", "31.1-jre")         // https://mvnrepository.com/artifact/com.google.guava/guava
         set("junitVersion", "5.8.2")            // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-api
         set("junitPioneerVersion", "1.5.0")     // https://mvnrepository.com/artifact/org.junit-pioneer/junit-pioneer
         set("mockitoVersion", "4.1.0")          // https://mvnrepository.com/artifact/org.mockito/mockito-junit-jupiter
