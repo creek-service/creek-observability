16cff5905ce79a13e16f61d1d2d19caeaac28d98
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2022-11-05 23:22:19 +0000

    update to the latest Creek release version (#75)

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,7 +65,7 @@ subprojects {
     project.version = project.parent?.version!!
 
     extra.apply {
-        set("creekVersion", "0.2.0-SNAPSHOT")
+        set("creekVersion", "0.2.0")
         set("spotBugsVersion", "4.7.3")         // https://mvnrepository.com/artifact/com.github.spotbugs/spotbugs-annotations
         set("slf4jVersion", "2.0.3")            // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
         set("log4jVersion", "2.19.0")           // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core
